7699af57169a7c179a0a3c7c39d11a36d0e135d5
Mehrdad <mehrdad.mmz.ca@gmail>
2025-02-16 22:14:11 -0800

    fixed some minor bugs

--- training.py
+++ training.py
@@ -3,8 +3,10 @@ from racetrack import Track
 from network import FirstNetwork
 import os
 
-car_image_paths = [os.path.join('images', f'car{i}.png') for i in range(5)]  # composing car image's paths
-canvas = Canvas(Track(), car_image_paths)  # passing the track and car images to the Canvas
+# composing car image's paths
+car_image_paths = [os.path.join('images', f'car{i}.png') for i in range(5)]
+# passing the track and car images to the Canvas
+canvas = Canvas(Track(), car_image_paths)
 
 population_count = 3
 networks = [FirstNetwork() for _ in range(population_count)]
