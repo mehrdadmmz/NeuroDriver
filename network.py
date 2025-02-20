678fd604aef6381a62cb137a3444eebb323da481
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-02-19 23:48:41 -0800

    Update network.py

--- network.py
+++ network.py
@@ -6,7 +6,8 @@ class Layer:
     def __init__(self, inputs_count, outputs_count):
         self.outputs = [0.0 for _ in range(outputs_count)]
         # 2D matrix for connecting each input to each output, each weight has a value between -1 and 1
-        self.weights = [[random.random() * 2 - 1 for _ in range(inputs_count)] for _o in range(outputs_count)]
+        self.weights = [[random.random() * 2 - 1 for _ in range(inputs_count)]
+                        for _o in range(outputs_count)]
 
     def feed_forward(self, inputs):
         # For each output neuron, the code loops through all inputs, multiplies each by the corresponding weight,
@@ -14,8 +15,10 @@ class Layer:
         for output_index, output in enumerate(self.outputs):
             total = 0
             for weight_index, inp in enumerate(inputs):
-                total += inp * self.weights[output_index][weight_index]  # total can have an arbitrarily large value
-            self.outputs[output_index] = math.tanh(total)  # for limiting the value between -1 and 1 --> activation func
+                # total can have an arbitrarily large value
+                total += inp * self.weights[output_index][weight_index]
+            # for limiting the value between -1 and 1 --> activation func
+            self.outputs[output_index] = math.tanh(total)
 
 
 class Network:
@@ -31,7 +34,5 @@ class Network:
             layer.feed_forward(inputs)
             # store the layer output back in the input variable to be used as the inputs for the next iteration
             inputs = [i for i in layer.outputs]
-        return self.layers[-1].outputs  # returning the outputs of the last layer
-
-
-
+        # returning the outputs of the last layer
+        return self.layers[-1].outputs
