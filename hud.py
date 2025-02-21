fbdccebc72dda84ab00b40cd83a84621aee30bf5
Mehrdad <mehrdad.mmz.ca@gmail>
2025-02-21 15:55:05 -0800

    added colors and values to the neurons

--- hud.py
+++ hud.py
@@ -8,6 +8,14 @@ class NeuronSprite:
         self.node_value = Label(x=x, y=y, color=(255, 255, 255, 255), font_size=12,
                                 anchor_x='center', anchor_y='center', batch=batch)
 
+    # update the value of the neuron, make it green if positive, red if negative, Values range from -1 to 1
+    def update(self, value):
+        self.node_value.text = f"{value:.2f}"
+        if value > 0:
+            self.node_fill.color = 0, int(value * 255), 0, 255
+        else:
+            self.node_fill.color = int(value * 200), 0, 0, 255
+
 
 # HUD: heads up display
 # The HUD class is responsible for displaying the current round, population, and speed of the cars.
@@ -26,7 +34,14 @@ class Hud:
                 y -= 50
             x += 50
 
-
-    def update(self, alive, population, speed):
+    def update(self, network, alive, population, speed):
         self.population_label.text = f"Population: {alive}/{population}"
         self.speed_label.text = f"Speed: {speed:.2f}"
+        index = 0
+        for input in network.inputs:  # starting from the input neurons
+            self.neurons[index].update(input)
+            index += 1
+        for layer in network.layers:  # continue with the hidden layers
+            for value in layer.outputs:
+                self.neurons[index].update(value)
+                index += 1
